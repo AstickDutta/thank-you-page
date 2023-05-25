9f8e8b84d8406d0c1faece5b10144299edb4b514
AstickDutta <astickdutta19@gmail.com>
2023-05-25 14:58:48 +0530

    final push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThankYouPage from './Components/ThankYouPage';
+import './styles.css'; 
+
+const App = () => {
+  return (
+    <div className="container">
+     
+      <ThankYouPage />
+    </div>
+  );
+};
+export default App;
---
+++ src/Components/ThankYouPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ThankYouPage = () => {
+  return (
+    <>
+      <div className="content">
+        <div className="wrapper-1">
+          <div className="wrapper-2">
+            <h1>Thank you !</h1>
+            <p>Thank you for subscribe.</p>
+          </div>
+        </div>
+      </div>
+      <link
+        href="https://fonts.googleapis.com/css?family=Kaushan+Script|Source+Sans+Pro"
+        rel="stylesheet"
+      />
+    </>
+  );
+};
+
+export default ThankYouPage;
